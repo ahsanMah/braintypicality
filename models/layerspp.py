da08dd501cf5b3b7bfe6cc5abd2f4ac88a271a5c
ahsanMah <mahmood.ahsan@outlook.com>
2023-03-02 21:35:35 +0000

    Only softmax in float32

--- models/layerspp.py
+++ models/layerspp.py
@@ -368,9 +368,10 @@ class AttentionBlock3d(nn.Module):
         with torch.cuda.amp.autocast(enabled=True, dtype=torch.float32):
             w = torch.einsum("b c q, b c k -> b q k", q, k) * self.scale
             w = F.softmax(w, dim=-1)
-            h = torch.einsum("b q k , b c k -> b c q", w, v)
-            h = torch.reshape(h, (B, C, H, W, D))
-        
+            # print("INSIDE ATTENTION BLOCK:", w.dtype)
+        h = torch.einsum("b q k , b c k -> b c q", w, v)
+        # print("OUTSIDE ATTENTION BLOCK:", h.dtype)
+        h = torch.reshape(h, (B, C, H, W, D))
         h = self.proj(h)
         x = x + h
         x = x * self.skip_scale
