+ Fixed inverse scaling that was ruining runs + Added dialtion args

models/layerspp.py
@@ -274,6 +274,7 @@ class ResnetBlockBigGANpp(nn.Module):
         dropout: float = 0.0,
         init_scale: float = 0.0,
         pre_conv: Any = None,
+        dilation: int = 1,
     ):
         super().__init__()
 
@@ -292,6 +293,7 @@ class ResnetBlockBigGANpp(nn.Module):
             in_channels=in_channels,
             out_channels=in_channels,
             kernel_size=kernel_size,
+            dilation=dilation,
         )
 
         if temb_dim is not None:
@@ -308,6 +310,7 @@ class ResnetBlockBigGANpp(nn.Module):
             out_channels=in_channels,
             init_scale=init_scale,
             kernel_size=kernel_size,
+            dilation=dilation,
         )
         self.dropout = nn.Dropout(dropout)
 
@@ -365,7 +368,7 @@ class AttentionBlock3d(nn.Module):
             w = torch.einsum("b c q, b c k -> b q k", q, k) * self.scale
             w = F.softmax(w, dim=-1)
             # print("INSIDE ATTENTION BLOCK:", w.dtype)
-        h = torch.einsum("b q k , b c k -> b c q", w, v)
+            h = torch.einsum("b q k , b c k -> b c q", w, v)
         # print("OUTSIDE ATTENTION BLOCK:", h.dtype)
         h = torch.reshape(h, (B, C, H, W, D))
         h = self.proj(h)

models/ncsnpp3d.py
@@ -164,6 +164,7 @@ class SegResNetpp(nn.Module):
                 act=self.act,
                 kernel_size=self.conv_size,
                 spatial_dims=self.spatial_dims,
+                dilation=self.dilation,
             )
 
         self.time_embed_layer = self._make_time_cond_layers(embedding_type)
@@ -208,6 +209,7 @@ class SegResNetpp(nn.Module):
         dense_1 = layerspp.make_dense_layer(sz * 2, sz * 2)
 
         layer_list.append(dense_0)
+        layer_list.append(torch.nn.SiLU())
         layer_list.append(dense_1)
 
         return nn.Sequential(*layer_list)
@@ -323,18 +325,19 @@ class SegResNetpp(nn.Module):
                 self.spatial_dims,
                 self.init_filters,
                 out_channels,
-                kernel_size=1,
+                kernel_size=3,
                 bias=True,
             ),
         )
 
     def forward(self, x, time_cond):
-        if self.resblock_pp and not self.data.centered:
-            # If input data is in [0, 1]
-            x = 2 * x - 1.0
+        # if self.resblock_pp and not self.data.centered:
+        #     # If input data is in [0, 1]
+        #     x = 2 * x - 1.0
 
         if self.fourier_features:
             z = self.fourier_encoder(x)
+            # print("FOURIER: ", z.dtype)
             x = torch.cat([x, z], dim=1)
 
         x = self.convInit(x)
@@ -348,6 +351,7 @@ class SegResNetpp(nn.Module):
             # Gaussian Fourier features embeddings.
             used_sigmas = time_cond
             temb = torch.log(used_sigmas)
+            # print("TEMBS: ", temb.dtype)
         elif self.embedding_type == "positional":
             # Sinusoidal positional embeddings.
             # TODO: Calculate sigmas
@@ -356,6 +360,7 @@ class SegResNetpp(nn.Module):
             temb = layers.get_timestep_embedding(timesteps, self.time_embedding_sz // 2)
 
         t = self.time_embed_layer(temb)
+        # print("After time embed: ", t.dtype)
 
         for i, down_block in enumerate(self.down_layers.values()):
             # for down_block in blocks: