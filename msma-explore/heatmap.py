b8b2b2c5ecbb7244eebee88995146c7a33880f94
ahsanMah <mahmood.ahsan@outlook.com>
2024-05-23 14:50:28 -0400

    cleaning heatmap

--- msma-explore/heatmap.py
+++ msma-explore/heatmap.py
@@ -1,6 +1,5 @@
 import glob
 import os
-import pdb
 import re
 from collections import defaultdict
 
@@ -34,7 +33,12 @@ opts.defaults(
 
 BOKEH_TOOLS = {"tools": ["hover", "box_select"]}
 D3_COLORS = d3["Category10"][10]
+
 COHORTS = ["ABCD", "LR-Typical", "HR-Inlier", "Atypical", "Down's Syndrome", "ASD +ve"]
+INLIER_COHORTS = ["ABCD", "LR-Typical"]
+OOD_COHORTS = [c for c in COHORTS if c not in INLIER_COHORTS]
+
+cohort_to_idx = {c: i for i, c in enumerate(COHORTS)}
 COHORT_COLORS = {c: D3_COLORS[i] for i, c in enumerate(COHORTS)}
 
 DATA_DIR = (
@@ -42,11 +46,7 @@ DATA_DIR = (
 )
 
 CACHE_DIR = "/ASD/ahsan_projects/braintypicality/dataset/template_cache/"
-
-
-procd_ref_img_path = f"{CACHE_DIR}/cropped_niral_mni.nii.gz"
 REF_BRAIN_IMG = np.load(f"{CACHE_DIR}/ref_brain.npy")
-# np.save(f"{CACHE_DIR}/ref_brain.npy", REF_BRAIN_IMG)
 REF_BRAIN_MASK = (REF_BRAIN_IMG > -1).astype(np.float32)
 REF_BRAIN_IMG = (REF_BRAIN_IMG + 1) / 2 * 100
 
@@ -66,7 +66,7 @@ SCORE_DIR = f"{WORKDIR}/ckpt_1500002/smin=0.01_smax=0.80_t=20/"
 HEATMAP_DIR = f"{WORKDIR}/heatmaps_v2/"
 
 LOAD_GLOBAL_SCORES = True
-GRID_ROWS, GRID_COLS = 11, 9
+GRID_ROWS, GRID_COLS = 9, 9
 
 
 def get_image_files_list(dataset_name: str, dataset_dir: str, splits_dir: str):
@@ -98,7 +98,9 @@ def get_image_files_list(dataset_name: str, dataset_dir: str, splits_dir: str):
 
 @pn.cache
 def get_region_scores():
-    region_scores = pd.read_csv(f"{HEATMAP_DIR}/roi/AAL+CSF_roi_scores.csv",index_col="ID")
+    region_scores = pd.read_csv(
+        f"{HEATMAP_DIR}/roi/AAL+CSF_roi_scores.csv", index_col="ID"
+    )
     cohort_renamer = {
         "IBIS-LR-Typical": "LR-Typical",
         "IBIS-HR-Typical": "HR-Inlier",
@@ -124,11 +126,15 @@ def get_ibis_metadata():
 @pn.cache
 def load_global_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     ### ABCD
-    score_file = np.load(f"{SCORE_DIR}/abcd-train_abcd-val_lesion_load_20-enhanced_results.npz", allow_pickle=True)
-    abcd_data = np.concatenate([score_file[f] for f in ['eval_score_norms', 'inlier_score_norms']], axis=0)
+    # score_file = np.load(f"{SCORE_DIR}/abcd-train_abcd-val_lesion_load_20-enhanced_results.npz", allow_pickle=True)
+    # abcd_data = np.concatenate([score_file[f] for f in ['inlier_score_norms']], axis=0)
 
-    score_file = np.load(f"{SCORE_DIR}/abcd-val_abcd-test_lesion_load_20_results.npz", allow_pickle=True)
-    abcd_data = np.concatenate([abcd_data, score_file['inlier_score_norms']], axis=0)
+    score_file = np.load(
+        f"{SCORE_DIR}/abcd-val_abcd-test_lesion_load_20_results.npz", allow_pickle=True
+    )
+    abcd_data = np.concatenate(
+        [score_file["eval_score_norms"], score_file["inlier_score_norms"]], axis=0
+    )
 
     ### IBIS
     score_file = np.load(
@@ -164,7 +170,6 @@ def load_global_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
 
     sample_ids = []
     for name in [
-        "abcd-train",
         "abcd-val",
         "abcd-test",
         "ibis-inlier",
@@ -176,9 +181,7 @@ def load_global_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         filenames = get_image_files_list(
             name,
             dataset_dir="/DATA/Users/amahmood/braintyp/processed_v2",
-            # splits_dir="/codespace/sade/sade/datasets/brains/",
             splits_dir="/ASD/ahsan_projects/braintypicality/dataset/dataset_split_builder/",
-
         )
         sids = [x["image"].split("/")[-1].replace(".nii.gz", "") for x in filenames]
         print(name, len(sids), len(set(sids)))
@@ -186,7 +189,7 @@ def load_global_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
             if s in sample_ids:
                 print(name, s)
         sample_ids.extend(sids)
-    
+
     sample_ids = np.array(sample_ids)
 
     # data normalization
@@ -236,7 +239,6 @@ def load_lobe_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
 
     sample_ids = []
     for name in [
-        # "abcd-train",
         "abcd-val",
         "abcd-test",
         "ibis-inlier",
@@ -248,7 +250,6 @@ def load_lobe_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         filenames = get_image_files_list(
             name,
             dataset_dir="/DATA/Users/amahmood/braintyp/processed_v2",
-            # splits_dir="/codespace/sade/sade/datasets/brains/",
             splits_dir="/ASD/ahsan_projects/braintypicality/dataset/dataset_split_builder/",
         )
         sids = [x["image"].split("/")[-1].replace(".nii.gz", "") for x in filenames]
@@ -264,7 +265,9 @@ def load_lobe_score_data() -> tuple[np.ndarray, np.ndarray, np.ndarray]:
 
 @pn.cache
 def build_simpsom_map(
-    data, net_height=7, net_width=7,
+    data,
+    net_height=7,
+    net_width=7,
 ):
     # Ideally ...
     # num_neurons = 5 * np.sqrt(data.shape[0])
@@ -355,7 +358,6 @@ async def plot_behavior_scores(index, col, bins=np.arange(-1, 101, 2)):
 
 
 def print_demographics(index):
-    # cohorts = df["Cohort"]
     selected = METADATA_DF[["Cohort", "Sex"]]
     sids = get_sids_from_selection_event(index) if len(index) > 0 else None
 
@@ -363,10 +365,9 @@ def print_demographics(index):
         selected = METADATA_DF.loc[sids]
 
     cohorts = selected.Cohort.value_counts()
-    print(cohorts)
     sexes = selected.Sex.value_counts()
     cohorts = cohorts.reindex(COHORTS[1:], fill_value=0)
-    sexes = sexes.reindex(['Male', 'Female'], fill_value=0)
+    sexes = sexes.reindex(["Male", "Female"], fill_value=0)
 
     md1 = sexes.to_frame().T.to_markdown().split("\n")
     md2 = cohorts.to_frame().T.to_markdown().split("\n")
@@ -374,13 +375,13 @@ def print_demographics(index):
     header = md1[0] + " - " + md2[0]
     alignrow = md1[1] + " - " + md2[1]
     valrow = md1[2] + " - " + md2[2]
-    mdtable = '\n'.join([header, alignrow, valrow])
+    mdtable = "\n".join([header, alignrow, valrow])
 
     return mdtable
 
 
 def plot_demographics(index):
-    selected = METADATA_DF[["Cohort","Sex"]]
+    selected = METADATA_DF[["Cohort", "Sex"]]
     sids = get_sids_from_selection_event(index) if len(index) > 0 else None
 
     if sids:
@@ -391,10 +392,8 @@ def plot_demographics(index):
     selected = selected[col_order]
 
     return selected.hvplot(kind="bar").opts(
-        height=ROI_PLOT_HEIGHT - HEATMAP_HEIGHT - 50,
-        title="Cohort Distribution"
+        height=ROI_PLOT_HEIGHT - HEATMAP_HEIGHT - 50, title="Cohort Distribution"
     )
-
 
 
 @pn.cache
@@ -410,9 +409,14 @@ def plot_roi_scores(index, quantile_threshold=60, show_bars_max=80):
     roi_median = roi_median[:show_bars_max]
     selected_rois = roi_median.index.to_list()
 
-    roi_data = sample_rois[selected_rois[::-1] + ["Cohort"]].reset_index(
-    ).melt(id_vars=["ID", "Cohort"], var_name="ROI", value_name="Percentile")
-    roi_data["Color"] = roi_data["ROI"].apply(lambda r: "pink" if roi_median[r] > quantile_threshold else "lightgrey")
+    roi_data = (
+        sample_rois[selected_rois[::-1] + ["Cohort"]]
+        .reset_index()
+        .melt(id_vars=["ID", "Cohort"], var_name="ROI", value_name="Percentile")
+    )
+    roi_data["Color"] = roi_data["ROI"].apply(
+        lambda r: "pink" if roi_median[r] > quantile_threshold else "lightgrey"
+    )
 
     boxplots = []
 
@@ -425,9 +429,7 @@ def plot_roi_scores(index, quantile_threshold=60, show_bars_max=80):
                 invert=True,
                 title="ROI Scores",
                 legend=False,
-            ).opts(
-                box_fill_color=color
-            )
+            ).opts(box_fill_color=color)
         )
 
     boxplot = hv.Overlay(boxplots)
@@ -460,7 +462,7 @@ def plot_roi_scores(index, quantile_threshold=60, show_bars_max=80):
             toolbar=None,
             labelled=["x"],
             yticks=0,
-        #     xlim=(0,100)
+            #     xlim=(0,100)
         )
     )
 
@@ -468,8 +470,10 @@ def plot_roi_scores(index, quantile_threshold=60, show_bars_max=80):
 
     boxplot = boxplot.relabel("ROI Scores").opts(
         opts.Overlay(
-            min_width=ROI_PLOT_WIDTH, min_height=ROI_PLOT_HEIGHT, show_legend=False,
-            xlim=(0,100)
+            min_width=ROI_PLOT_WIDTH,
+            min_height=ROI_PLOT_HEIGHT,
+            show_legend=False,
+            xlim=(0, 100),
         )
         # height=ROI_PLOT_HEIGHT
     )
@@ -530,39 +534,34 @@ def image_slice(ref_slice, heatmap_slice, lbrt, mapper, thresh=20):
     )
 
 
-######
-
-
-# Create data sources for the plots
+###### Create data sources for the plots ########
 ibis_metadata = get_ibis_metadata()
 region_scores = get_region_scores()
 das_cols = [c for c in ibis_metadata.columns if "DAS" in c]
 cbcl_cols = list(
     filter(
-        lambda c: re.match(".*percentile", c),
+        lambda c: re.match(".*_T_score", c),
         ibis_metadata.columns,
     )
 )
 vineland_cols = list(
-    filter(lambda c: re.match(".*Vine.*PERCENTILE", c), ibis_metadata.columns)
+    filter(lambda c: re.match(".*Vine.*STD_SCORE", c), ibis_metadata.columns)
 )
 ados_cols = list(filter(lambda c: re.match(".*ADOS.*", c), ibis_metadata.columns))
+#################################################
 
-########## SOM ##########
-data, score_target, sample_ids = load_global_score_data() if LOAD_GLOBAL_SCORES else load_lobe_score_data()
-inlier_data = data[score_target < 2]
-# som = build_som_map(
-#     inlier_data, m_neurons=GRID_ROWS, n_neurons=GRID_COLS, max_iters=5000
-# )
-# distance_map = umatrix = som.distance_map()
-# weights = som.get_weights()
 
-# abcd_data = data[score_target == 0]
-# ibis_typical = data[score_target == 1]
-# abcd_DA_data = domain_adapted_subsample(abcd_data, ibis_typical, n_subsamples=1000)
-# inlier_data = np.concatenate([abcd_DA_data, ibis_typical])
+########## SOM ############
+data, score_target, sample_ids = (
+    load_global_score_data() if LOAD_GLOBAL_SCORES else load_lobe_score_data()
+)
+inlier_labels_idx = [cohort_to_idx[c] for c in INLIER_COHORTS]
+inlier_mask = np.isin(score_target, inlier_labels_idx)
+inlier_data = data[inlier_mask]
 
-som, distance_map = build_simpsom_map(inlier_data, net_height=GRID_ROWS, net_width=GRID_COLS)
+som, distance_map = build_simpsom_map(
+    inlier_data, net_height=GRID_ROWS, net_width=GRID_COLS
+)
 bmus = [som.nodes_list[int(mu)].pos for mu in som.find_bmu_ix(data)]
 ###########################
 
@@ -581,14 +580,13 @@ for idx in range(0, data.shape[0]):
         # SOM ASD+ves also have DS - we effectively keep the DS as the label
         print(f"{sid} already exists at {(wx, wy)} with label {COHORTS[label_idx]}")
         continue
-    
+
     scatter_data["x"].append(wx)
     scatter_data["y"].append(wy)
     scatter_data["Cohort"].append(COHORTS[score_target[idx]])
     scatter_data["ID"].append(sid)
 
-
-    if COHORTS[label_idx]  != "ABCD":
+    if COHORTS[label_idx] not in INLIER_COHORTS:
         grid_to_sample[(wx, wy)].append(sid)
 
 
@@ -596,7 +594,7 @@ scatter_df = pd.DataFrame(scatter_data)
 scatter_df.set_index("ID")
 METADATA_DF = pd.merge(scatter_df, ibis_metadata, on="ID").set_index("ID")
 
-#### Rectangles at x y coords
+############ Rectangles at x y coords ##################
 cell_groups = METADATA_DF[["x", "y", "Cohort"]].groupby(["x", "y"])
 rects = []
 # xs = np.linspace(0,1,4, endpoint=False)
@@ -619,13 +617,14 @@ for pos, cell in cell_groups:
     x1 = xs[1:] + x
     y0 = ys + y
     y1 = ys + y
-    for i, c in enumerate(COHORTS[1:]):
+    for i, c in enumerate(OOD_COHORTS):
         if c in counts:
             y1[i] += (counts[c] / maxcount - ypad) * scaler
         cohorts_at_pos.append(c)
 
     rects.extend(list(zip(x0, y0, x1, y1, cohorts_at_pos)))
 histograms = hv.Rectangles(rects, vdims="cohorts")
+########################################################
 
 # For plotting the background heatmap
 heatmap_data = defaultdict(list)
@@ -640,13 +639,15 @@ for j in range(GRID_COLS):
         heatmap_data["x"].append(j)
         heatmap_data["y"].append(i)
         # not a real density
-        heatmap_data["density"].append(1 - normed_dist[j,i])
-        
+        heatmap_data["density"].append(1 - normed_dist[j, i])
+
         counts = {}
         if (j, i) in cell_groups.groups:
-            counts.update(cell_groups.get_group((j, i))["Cohort"].value_counts().to_dict())
+            counts.update(
+                cell_groups.get_group((j, i))["Cohort"].value_counts().to_dict()
+            )
         # print(j,i, counts)
-        for cohort in COHORTS[1:]:
+        for cohort in OOD_COHORTS:
             heatmap_data[cohort].append(counts.get(cohort, 0))
 
 heatmap_df = pd.DataFrame(heatmap_data)
@@ -663,6 +664,7 @@ heatmap_selection = streams.Selection1D(source=heatmap_base)
 # Declare widgets
 
 # make a function that builds a plot and widget for provided columns
+
 
 def build_behavior_plot(plot_name, cols, bins=np.arange(-2, 101, 2)):
 
@@ -724,7 +726,7 @@ update_current_volume()
 volpane = pn.pane.VTKVolume(
     CURRENT_VOLUME,
     max_height=IMG_HEIGHT,
-    max_width=IMG_WIDTH-50,
+    max_width=IMG_WIDTH - 50,
     display_slices=False,
     # colormap="Black-Body Radiation",
     render_background="black",
@@ -745,6 +747,8 @@ def threshold_volume_object(thresh):
 def update_volume_object(selection_event):
     update_current_volume(index=selection_event.new)
     threshold_volume_object(select_vol_thresh_widget.value)
+
+
 heatmap_selection.param.watch(update_volume_object, "index", queued=True)
 
 
@@ -761,15 +765,19 @@ dmap_k = hv.DynamicMap(pn.bind(image_slice_k, sk=volpane.param.slice_k, **common
 
 #### Bind the functions to the selection stream
 
-behaviour_view = pn.GridSpec(
-    min_width=ROI_PLOT_WIDTH + HEATMAP_WIDTH, ncols=2, nrows=3
-)
+behaviour_view = pn.GridSpec(min_width=ROI_PLOT_WIDTH + HEATMAP_WIDTH, ncols=2, nrows=3)
 
-bplots_config = {"DAS": [das_cols], "CBCL": [cbcl_cols],
-                 "Vineland": [vineland_cols],
-                 "ADOS": [ados_cols,
-                           np.arange(METADATA_DF[ados_cols].values.min(), METADATA_DF[ados_cols].values.max(), 2)]
-                }
+bplots_config = {
+    "DAS": [das_cols],
+    "CBCL": [cbcl_cols],
+    "Vineland": [vineland_cols],
+    "ADOS": [
+        ados_cols,
+        np.arange(
+            METADATA_DF[ados_cols].values.min(), METADATA_DF[ados_cols].values.max(), 2
+        ),
+    ],
+}
 
 bplots = []
 for i, name in enumerate(bplots_config):
@@ -785,7 +793,11 @@ roi_plot = pn.bind(plot_roi_scores, index=heatmap_selection.param.index)
 
 
 base_plot = (heatmap_base * histograms).opts(
-    opts.HeatMap(tools=["hover", "box_select", "tap"], cmap="Blues", colorbar=True,),
+    opts.HeatMap(
+        tools=["hover", "box_select", "tap"],
+        cmap="Blues",
+        colorbar=True,
+    ),
     opts.Rectangles(
         color="cohorts",
         cmap=COHORT_COLORS,
@@ -803,7 +815,6 @@ base_plot = (heatmap_base * histograms).opts(
         axiswise=False,
         shared_axes=False,
         framewise=False,
-        # **BOKEH_TOOLS,
     ),
 )
 
@@ -835,10 +846,20 @@ controller = volpane.controls(
         # "edge_gradient",
     ],
 )
-vol_widget = pn.WidgetBox("## Controls", select_vol_thresh_widget, controller,
-                           max_width=350, sizing_mode="stretch_both")
+vol_widget = pn.WidgetBox(
+    "## Controls",
+    select_vol_thresh_widget,
+    controller,
+    max_width=350,
+    sizing_mode="stretch_both",
+)
 
-gspec = pn.GridSpec(width=1600, height=1000, ncols=9, nrows=5,)
+gspec = pn.GridSpec(
+    width=1600,
+    height=1000,
+    ncols=9,
+    nrows=5,
+)
 
 gspec[:2, :2] = base_plot
 gspec[:3, 4:6] = vol_widget
@@ -860,6 +881,6 @@ volume_view = gspec
 layout = pn.Tabs(
     ("Explorer", explorer_view),
     ("Brain Volumes", pn.panel(volume_view, defer_load=True)),
-    dynamic=False,
+    dynamic=True,
 )
 layout.servable()
